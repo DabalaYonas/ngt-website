50b9a31b94dc363b349b0a5ba064b7f77477e740
Dabala <dabo.yonasl@gmail.com>
2026-03-02 13:02:06 +0300

    Updated testimonials

--- lib/data/testimonials.ts
+++ lib/data/testimonials.ts
@@ -20,24 +20,4 @@ export const testimonials: Testimonial[] = [
       "I am thrilled to commend Next General Trading for their exceptional IT and construction services. Their expertise has significantly enhanced our campus infrastructure and technological capabilities. The professionalism, quality, and reliability they bring to every project have made a profound impact on our institution. I highly recommend Next General Trading for their outstanding commitment to excellence.",
     rating: 5,
   },
-  {
-    id: "2",
-    name: "Leta Tesfaye",
-    title: "Dr.",
-    role: "Former President",
-    company: "Dembi Dollo University",
-    content:
-      "I am thrilled to commend Next General Trading for their exceptional IT and construction services. Their expertise has significantly enhanced our campus infrastructure and technological capabilities. The professionalism, quality, and reliability they bring to every project have made a profound impact on our institution. I highly recommend Next General Trading for their outstanding commitment to excellence.",
-    rating: 5,
-  },
-  {
-    id: "3",
-    name: "Leta Tesfaye",
-    title: "Dr.",
-    role: "Former President",
-    company: "Dembi Dollo University",
-    content:
-      "I am thrilled to commend Next General Trading for their exceptional IT and construction services. Their expertise has significantly enhanced our campus infrastructure and technological capabilities. The professionalism, quality, and reliability they bring to every project have made a profound impact on our institution. I highly recommend Next General Trading for their outstanding commitment to excellence.",
-    rating: 5,
-  },
 ]
