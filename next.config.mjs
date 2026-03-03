d080df5f743c3ae4a93426eecfd43851d1b937fa
Dabala <dabo.yonasl@gmail.com>
2026-03-04 00:09:24 +0300

    Update for seo

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,35 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/index.php/contact',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/index.php/projects',
+        destination: '/projects',
+        permanent: true,
+      },
+      {
+        source: '/index.php/about',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/index.php/services',
+        destination: '/services',
+        permanent: true,
+      },
+      {
+        source: '/index.php/:path*',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
